09bf60c8fa5068ba79b8207a84b49d4fc04b5767
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-06-19 16:02:53 -0400

    Added more strings classifiers items for the metadata package

--- setup.py
+++ setup.py
@@ -4,26 +4,50 @@ import os
 version = '0.1'
 
 long_description = (
-    open('README.txt').read()
+    open('README.rst').read()
     + '\n' +
-    'Contributors\n'
-    '============\n'
-    + '\n')
+    open('CHANGES.txt').read()
+    + '\n' +
+    'Contributors\n' +
+    '============\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n'
+)
 
 setup(name='medialog.mobilethemeThree',
       version=version,
       description="A diazo theme for zettwerk.mobiletheming",
       long_description=long_description,
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: Theme",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='mobiletheming plone zetterk',
       author='Espen Moe-Nilssen',
       author_email='espen@medialog.no',
+      maintainer='Leonardo Caballero',
+      maintainer_email='leonardocaballero@gmail.com',
       url='http://github.com/espenmn/medialog.mobilethemeThree',
-      license='gpl',
+      license='GPL',
       packages=find_packages(),
       namespace_packages=['medialog'],
       include_package_data=True,
